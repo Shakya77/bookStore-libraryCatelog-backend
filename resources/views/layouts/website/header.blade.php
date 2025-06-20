0ddd41dbf60698cc2e7c95b5a1b3ab2fd9c8b16a
shakya77 <bijanshakya145@gmail.com>
2025-06-20 14:48:08 +0545

    made some changes

---
+++ resources/views/layouts/website/header.blade.php
@@ -0,0 +1,16 @@
+<header
+    class="bg-white dark:bg-gray-900 text-nepal-blue py-4 dark:text-gray-200 flex justify-center px-6">
+    <div class="max-w-[1600px] w-full">
+        <div class="flex items-center justify-between">
+            <button id="nav_themeToggle" class="p-1.5 rounded-full hover:bg-white/20 transition-colors"
+                aria-label="Toggle theme">
+                <span class="dark-icon hidden">
+                    <span class="iconify" data-icon="tabler:moon-filled" data-width="18"></span>
+                </span>
+                <span class="light-icon">
+                    <span class="iconify" data-icon="tabler:sun-filled" data-width="18"></span>
+                </span>
+            </button>
+        </div>
+    </div>
+</header>
